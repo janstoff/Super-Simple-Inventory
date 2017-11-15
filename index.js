82111124a63b9bc30ab22f4452d911d7f96afdc3
Jan Steinhoff <jan.steinhoff@gmail.com>
2017-11-15 16:25:55 +0100

    basic auth set up for first test

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+//make passport aware of new GoogleStrategy
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		accessToken => {
+			console.log(accessToken)
+		}
+	)
+)
 
 const PORT = process.env.PORT || 5000
 //heroko injects environment variables in this case the port,
 //while there is no port, i.e. dev mode we use localhost:5000
-
 app.listen(PORT)
